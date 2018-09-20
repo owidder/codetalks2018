06ffb8f19ad5c4b22073f6039930846cf4c1427d
Oliver Widder <oliver.widder@gmail.com>
2018-09-20 18:10:47 +0200

    tmp

--- truffle/truffle.js
+++ truffle/truffle.js
@@ -1,23 +1,18 @@
-/*
- * NB: since truffle-hdwallet-provider 0.0.5 you must wrap HDWallet providers in a 
- * function when declaring them. Failure to do so will cause commands to hang. ex:
- * ```
- * mainnet: {
- *     provider: function() { 
- *       return new HDWalletProvider(mnemonic, 'https://mainnet.infura.io/<infura-key>') 
- *     },
- *     network_id: '1',
- *     gas: 4500000,
- *     gasPrice: 10000000000,
- *   },
- */
+var HDWalletProvider = require("truffle-hdwallet-provider");
+var mnemonic = "season divert corn exchange harbor book boat allow vivid error apple process";
 
 module.exports = {
-  networks: {
-    development: {
-      host: "localhost",
-      port: 8545,
-      network_id: "*" // Match any network id
-    }
-  }
+    networks: {
+        development: {
+            host: "localhost",
+            port: 8545,
+            network_id: "*" // Match any network id
+        },
+        infura: {
+            provider: function () {
+                return new HDWalletProvider(mnemonic, "https://rinkeby.infura.io/v3/6b901e50131a4b69a46c6aad0294cb1d")
+            },
+            network_id: 4,
+        },
+    },
 };
